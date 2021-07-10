cb98a30685e9ae6681c099d4387f078ddb3ffe9b
Oles <olesservetnyk@gmail.com>
2021-07-10 17:11:20 +0300

    Delete Config.cs
    config.cs removed

--- Config.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace telacubot
-{
-    public static class Config
-    {
-        public static readonly string token = "1853379149:AAHpRU-7neC3qw3LS3YKPZZkjdJtQpevzM4";
-    }
-}
